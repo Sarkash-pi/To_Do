62d8a4176f2987fc84900e223e60d2ae7ebd316b
Sarkash Talib <Sarkash.talib@gmail.com>
2021-05-08 19:53:05 +0100

    adding delete page

--- src/todo/views.py
+++ src/todo/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .forms import TodoForm
 from .models import Todo
 
+# FBV (funcion based views)
+
 
 def list_todo(request):
     todos = Todo.objects.all()
@@ -30,4 +32,10 @@ def update_todo(request, pk):
 
 
 def delete_todo(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+
+    if request.method == "POST":
+        todo.delete()
+        return redirect("todo:list-todo")
+
     return render(request, "delete_todo.html", context={})
